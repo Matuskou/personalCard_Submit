3498bc7872146026017f6d1d50377aaad28ef143
Matuskou <wolfenzi92@gmail.com>
2023-02-02 21:32:15 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,140 @@
+<template>
+    <main>
+      <h1>Zoznam ľudí</h1>
+      <section class="list">
+        <section v-for="person of persons" v-bind:key="person"
+        v-bind:class="{'card-container':true,'card':true,'adult': calculateAge(person)>=18,'elders':calculateAge(person)>=70,'youngsters':calculateAge(person)<=10}"><!--v tomto prípade clase priradíme objekt, ktorého vlastnosti predstavujú jednotlivé triedy a ich hodnoti predstavujú bool vyjadrujúci, kedy sa má daná trieda pridať danému elemntu-->
+        <personCard @editWasClicked="editPerson" @removeWasClicked="removePerson" v-bind:person = "person"></personCard>
+        </section>
+    </section>
+    <div v-on:click="deleteAll" class="btn" v-if="bool == true">Delete All</div>
+    <input v-model="name" placeholder="name">
+	<div v-on:click="submit" class="btn-submit" v-if="bool == true">Submit</div>
+    <div>{{name}}</div>
+    </main>
+</template>
+
+<script setup>
+
+function calculateAge({birthday}){
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+    
+    return currentYear - birthday;
+}
+
+
+let initialPersons = [
+        {
+            fname: "Magdalena",
+            lname: "Magdova",
+            day: 11,
+            month: 1,
+            birthday: 1877,
+
+        },
+        {
+            fname: "Sofia",
+            lname: "Zlatkovicova",
+            day: 31,
+            month: 12,
+            birthday: 2015,
+            
+        },
+        {
+            fname: "Hugo",
+            lname: "Trubic",
+            birthday: 2008,
+            day: 6,
+            month: 4,
+            
+        },
+        {
+            fname: "Miki",
+            lname: "Spis",
+            day: 18,
+            month: 10,
+            birthday: 2012,
+        
+        },
+        {
+            fname: "Andrea",
+            lname: "Veresova",
+            day: 8,
+            month: 9,
+            birthday: 1888,
+            
+        },
+        {
+            fname: "Nora",
+            lname: "Mojsejova",
+            day: 12,
+            month: 7,
+            birthday: 1966,
+         
+        }, 
+    ]
+
+    let bool = ref(true)
+    let persons = ref(initialPersons)
+	let thisCard
+    const deleteAll = () => {
+        persons.value = []
+        bool = false
+        name.value=""
+    }
+
+    const removePerson = (personShouldBeRemoved) => {
+        persons.value=persons.value.filter(item=>item!=personShouldBeRemoved)
+    }
+
+	const editPerson = (personShouldBeEdited) => {
+        persons.value.forEach((item) => {
+            if(item == personShouldBeEdited){
+                name.value=item.fname
+
+                thisCard = item
+				name.value=" "
+			}
+        })
+    }
+
+	const submit = () => {
+        persons.value.forEach((item) => {
+            if(item == thisCard){
+                item.fname=name.value
+            }
+        })
+    }
+
+    let name = ref("")
+
+	
+</script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;500;600;700&display=swap');
+    body {
+        font-size: 10px;    
+        font-family: 'Oswald', sans-serif;
+        padding: 1rem;
+        background: linear-gradient(rgba(0, 47, 255, 0.781),rgb(160, 11, 152));
+        background-attachment: fixed;
+    }
+    main {
+        display: flex;
+        flex-direction: column;
+        gap: .75rem;
+    }
+    .list {
+        display: flex;
+        gap: 20px;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: stretch;
+        font-size: 1.2rem;  
+    }
+    .card-container {
+        flex-grow: 1;
+    }
+</style>
---
+++ components/personCard.vue
@@ -0,0 +1,112 @@
+<script setup>
+
+defineProps(['person'])
+
+function calculateAge({birthday}){
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+    
+    return currentYear - birthday;
+}
+
+</script>
+
+<template>
+    <section>
+          <article class="info">
+            <div class="name">{{person.fname}} {{person.lname}}</div>
+            <div class="birthday">{{person.day}}.{{person.month}}. {{person.birthday}} </div>
+            <div class="age">{{calculateAge(person)}}<span class="year-title">year</span></div>
+          </article>
+          <footer class="footer-card">
+            <span v-on:click="$emit('editWasClicked', person)" class="btn edit">Edit</span>
+            <span v-on:click="$emit('removeWasClicked', person)" class="btn remove">Remove</span>
+          </footer>
+        </section>
+</template>
+
+<style>
+
+.card {
+        background: rgba(2, 2, 2, 0.344);
+        border: 1px solid #bbb;
+        color: #444;
+        box-shadow: 0 3px 5px rgba(0,0,0,.2);
+        border-top-width: 5px;
+        border-top-color: rgb(11, 103, 113);
+        border-radius: 0px 15px 0px 15px;
+    }
+    .card.adult {
+        border-top-color: rgb(98, 0, 255);
+    }
+
+    .card.elders{
+
+        border-top-color: rgb(121, 53, 7);
+    }
+
+    .card.youngsters{
+
+        border-top-color: rgb(0, 255, 0);
+    }
+
+    .card:hover {
+        background-color: rgb(168, 168, 168);
+    }
+    .info {
+        padding: 1rem;
+        text-align: center;
+        font-size: 1.3rem;
+    }
+    .name {
+        color: rgba(255, 255, 255, 0.445);
+
+    }
+    .birthday {
+        font-weight: 300;
+        font-size: 1rem;
+    }
+
+    .age {
+        font-weight: 300;
+        font-size: 1rem;
+    }
+    .year-title {
+        color: #ffffffd3;
+    }
+    .footer-card {
+        display: flex;
+    }
+    .btn {
+        background: red;
+        padding: .75rem 2rem;
+        color: rgba(255, 255, 255, 0.7);
+        width: 50px;
+        flex-grow: 1;
+        text-align: center;
+        transition: .5s;
+        font-size: 1rem;
+        border-radius: 0px 15px 0px 15px;
+    }
+	.btn-submit {
+		background: rgb(0, 255, 47);
+        padding: .75rem 2rem;
+        color: rgba(255, 255, 255, 0.7);
+        width: 50px;
+        flex-grow: 1;
+        text-align: center;
+        transition: .5s;
+        font-size: 1rem;
+        border-radius: 0px 15px 0px 15px;
+	}
+    .btn.edit {
+        background: rgb(0, 200, 255);
+    }
+    .btn.remove {
+        background: rgba(255, 0, 0, 0.23);
+    }
+    .btn:hover {
+        opacity: .3;
+        cursor: pointer;;
+    }
+</style>
